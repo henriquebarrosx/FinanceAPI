a347dacbf726f7203e4ee6bf3bf07d5e47d7d631
Henrique Barros <henriquebarrosx@gmail.com>
2023-07-10 12:09:07 -0300

    funcionalidade: criação das entidades usuário e transação

--- src/domain/entities/transaction.ts
+++ src/domain/entities/transaction.ts
@@ -1,1 +1,11 @@
-export class Transaction { }
+export class Transaction {
+    constructor(
+        private userId: string,
+        private date: string,
+        private type: string,
+        private value: number,
+        private description: string,
+        private createdAt: string,
+        private updatedAt: string,
+    ) { }
+}
